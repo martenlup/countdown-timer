1677d29a0b9d2bfd0cd5bd2b6c22f0e0511eea1d
marten <marten.lupas@gmail.com>
2021-04-09 23:06:48 +0200

    added js div connection & formatTime function

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
 const atzenTag = "25 Sep 2021";
 
-function countdown(){
+function countdown() {
     const atzenTagDate = new Date(atzenTag);
     const currentDate = new Date();
 
-    const totalSeconds = (atzenTagDate- currentDate) / 1000;
+    const totalSeconds = (atzenTagDate - currentDate) / 1000;
 
-    const day = Math.floor(totalSeconds / 60 / 60 / 24);
-    const hour = Math.floor(totalSeconds / 60 / 60) % 24;
-    const min = Math.floor(totalSeconds / 60) % 60;
-    const sec = Math.floor(totalSeconds) % 60;
+    const days = Math.floor(totalSeconds / 60 / 60 / 24);
+    const hours = Math.floor(totalSeconds / 60 / 60) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
 
+    daysEl.innerHTML = formatTime(days);
+    hoursEl.innerHTML = formatTime(hours);
+    minutesEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
 }
+
+function formatTime(time) {
+    if (time < 10) {
+        return `0${time}`
+    }
+    else {
+        return time;
+    }
+}
+
 countdown();
 setInterval(countdown, 1000);
